a7c146fa492fc02e057dad091de74aa7f736584d
ttodawg01 <tonycampos1803@gmail.com>
2022-11-15 09:09:19 -0600

    updated forms

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,8 @@ from flask import Flask
 
 
 app = Flask(__name__)
+#add a secret key to app config
+app.config['SECRET_KEY'] = 'Never-guess'
+
 
 from . import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import SignUpForm
 
 @app.route('/')
 def index():
@@ -8,3 +9,8 @@ def index():
 @app.route('/posts')
 def post():
     return render_template("posts.html")
+
+@app.route('/signup')
+def signup():
+    form = SignUpForm()
+    return render_template('signup.html', form=form)
