aef06844d1fdbc1973ff10d4ddee7d0693620383
ttodawg01 <tonycampos1803@gmail.com>
2022-11-15 17:57:08 -0600

    updating form validation

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 #add a secret key to app config
-app.config['SECRET_KEY'] = 'Never-guess'
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 
 from . import routes
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app
 from app.forms import SignUpForm
 
@@ -10,7 +10,18 @@ def index():
 def post():
     return render_template("posts.html")
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
     form = SignUpForm()
+    if form.validate_on_submit():
+        print('success')
+        #data from the form
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(email, username, password)
+
+        #go back to the main page
+        return redirect(url_for('index'))
+
     return render_template('signup.html', form=form)
